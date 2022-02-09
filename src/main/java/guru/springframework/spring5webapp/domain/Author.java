6c97f813ee7b21cab946dcd1e2f2c1073d7e6135
aiman shakeel <aimanshakeel1008@gmail.com>
2022-02-09 18:34:22 +0530

    second commit with application properties change to enable h2 database consle and fixing Author POJO issue

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -19,7 +19,7 @@ public class Author {
 	public Author(){
 	}
 
-	public Author(String firstName, String lastNameks) {
+	public Author(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
